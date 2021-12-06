d963a6aef6802ea6162bcf08cae1161da6933424
JulietBangay <julietabangay@gmail.com>
2021-12-06 12:16:18 +0000

    Removed unrequired code

--- src/script.js
+++ src/script.js
@@ -165,10 +165,8 @@ function searchCity(event) {
 }
 
 function displayWeatherForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "f85062d84430cd35a6b8db439bd6c8f6";
   let forecastApi = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  console.log(forecastApi);
   axios.get(forecastApi).then(displayForecast);
 }
 
